7f4c9fd59ea224ec012b83919cfe3460d5b58050
Ann Wallace <annerz@gmail.com>
2016-01-28 18:05:57 -0800

    Initial Commit

---
+++ BasicWeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicWeatherApp
+//
+//  Created by annwa on 1/28/16.
+//  Copyright © 2016 ann wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicWeatherApp
+//
+//  Created by annwa on 1/28/16.
+//  Copyright © 2016 ann wallace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
